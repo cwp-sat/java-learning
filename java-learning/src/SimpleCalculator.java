1094439de2a5570162bb3ec8fe15a299c071d6af
cwpsat <15518255705@163.com>
2026-06-30 20:34:57 +0800

    first commit: HelloWorld

---
+++ java-learning/src/SimpleCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number：");
+        double num1 = sc.nextDouble();
+        System.out.println("choose + - * /");
+        char ch1 = sc.next().charAt(0);
+        System.out.println("Enter second number：");
+        double num2 = sc.nextDouble();
+        if(ch1=='+'){
+            System.out.println("answer is "+(num1+num2));
+        }
+        else if (ch1=='-') {
+            System.out.println("answer is "+(num1-num2));
+        }
+        else if (ch1=='*') {
+            System.out.println("answer is "+(num1*num2));
+        }
+        else if (ch1=='/') {
+            System.out.println("answer is "+(num1/num2));
+        }
+    }
+}
+
