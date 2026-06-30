83db499f72ddc3f2bf08607431f39407d13b2e62
cwpsat <15518255705@163.com>
2026-06-30 17:39:02 +0800

    first commit: HelloWorld

---
+++ java-learning/src/ValueDemo1.java
@@ -0,0 +1,28 @@
+public class ValueDemo1 {
+    public static void main(String[] args) {
+//        整数
+        System.out.println(666);
+        System.out.println(-777);
+//        小数
+        System.out.println(321.222);
+        System.out.println(-1.23);
+//        字符串
+        System.out.println("java-learning");
+        System.out.println("布尔可满足性问题");
+//        字符
+        System.out.println('男');
+        System.out.println('女');
+//        布尔类型
+        System.out.println(true);
+        System.out.println(false);
+//        空
+//        null不能直接打印，只能用字符串形式打印
+        System.out.println("null");
+//        特殊字符
+//        \n换行
+        System.out.println("123\n456");
+//        \t制表符
+        System.out.println("123"+"456");
+        System.out.println("123"+'\t'+"456");
+    }
+}
