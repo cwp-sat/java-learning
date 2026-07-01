c23a650adf6c4d4a1b20bd72aaa9562f121cd98b
cwpsat <15518255705@163.com>
2026-07-01 15:14:21 +0800

    first commit: HelloWorld

--- java-learning/src/learning/MultiplicationTable.java
+++ java-learning/src/learning/MultiplicationTable.java
@@ -1,6 +1,6 @@
 package learning;
 
-public class MultiplicatioTable {
+public class MultiplicationTable {
     public static void main(String[] args) {
         int i,j;
         for (i=1;i<=9;i++){
