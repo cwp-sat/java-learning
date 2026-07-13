5f2c716c9bf31b9a220ea64baad93254c2f9c20a
cwpsat <15518255705@163.com>
2026-07-13 10:39:38 +0800

    first commit: HelloWorld

--- java-learning/src/LeetCode/SumOfTwoNumbers.java
+++ java-learning/src/LeetCode/SumOfTwoNumbers.java
@@ -1,24 +1,33 @@
 package LeetCode;
 
-//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
-//你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
-//你可以按任意顺序返回答案。
-//示例 1：
-//输入：nums = [2,7,11,15], target = 9
-//输出：[0,1]
-//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
-
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the arrys like:1,2,3:");
-        int n=sc.nextInt();
-        int[] arr1 = new int[n];
-
-        System.out.println(Arrays.toString(arr1));
+        ArrayList<Integer>list = new ArrayList<>();
+        System.out.println("请输入数字:(按回车结束) ");
+        while(true){
+            String line=sc.nextLine();
+            if(line.isEmpty())break;
+            int num=Integer.parseInt(line);
+            list.add(num);
+        }
+        System.out.println("请输入目标数字：");
+        int target=sc.nextInt();
+        boolean flag=false;
+        for(int i=0;i<list.size();i++){
+            for (int j = i+1;j<list.size();j++){
+                if(list.get(i)+list.get(j)==target){
+                    flag=true;
+                    System.out.println("["+i+","+j+"]");
+                }
+            }
+        }
+        if(!flag){
+            System.out.println("没有合适的答案");
+        }
 
     }
 }
