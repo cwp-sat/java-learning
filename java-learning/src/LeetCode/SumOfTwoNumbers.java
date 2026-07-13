4ffc5b6d627a3c08a009e862c0b2224d337dc74a
cwpsat <15518255705@163.com>
2026-07-13 11:02:48 +0800

    first commit: HelloWorld

--- java-learning/src/LeetCode/SumOfTwoNumbers.java
+++ java-learning/src/LeetCode/SumOfTwoNumbers.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class SumOfTwoNumbers {
@@ -28,6 +29,22 @@ public class SumOfTwoNumbers {
         if(!flag){
             System.out.println("没有合适的答案");
         }
-
+//        class Solution {
+//            public int[] twoSum(int[] nums, int target) {
+//                HashMap<Integer,Integer>map=new HashMap<>();
+//                for (int i = 0; i < nums.length; i++) {
+//                    int current=nums[i];
+//                    int complement=target-current;
+//                    if(map.containsKey(complement)){
+//                        return new int[]{map.get(complement),i};
+//                    }else {
+//                        map.put(current,i);
+//                    }
+//                }
+//                return new int[]{-1,-1};
+//            }
+//        }
     }
 }
+
+
